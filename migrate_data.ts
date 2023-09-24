46e4be1437e3ff94a259cce77eecd6cf360d2246
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2023-09-24 16:09:33 +0200

    Commented migration

--- migrate_data.ts
+++ migrate_data.ts
@@ -5,26 +5,26 @@ import type { INestApplicationContext } from '@nestjs/common';
 
 async function migrateData() {
   // console.log('Data migration is not needed. Completed successfully');
-  let application: INestApplicationContext;
+  // let application: INestApplicationContext;
   try {
-    application = await NestFactory.createApplicationContext(AppModule);
-    const receivingsService = application.get(ReceivingsService);
-    const receivings = await receivingsService.findAll();
-    for (const receiving of receivings) {
-      await receivingsService.update({
-        id: receiving._id.toString(),
-        newData: {
-          client: receiving.client._id.toString(),
-          records: receiving.records,
-          timestamp: receiving.timestamp,
-        },
-      });
-    }
+    // application = await NestFactory.createApplicationContext(AppModule);
+    // const receivingsService = application.get(ReceivingsService);
+    // const receivings = await receivingsService.findAll();
+    // for (const receiving of receivings) {
+    //   await receivingsService.update({
+    //     id: receiving._id.toString(),
+    //     newData: {
+    //       client: receiving.client._id.toString(),
+    //       records: receiving.records,
+    //       timestamp: receiving.timestamp,
+    //     },
+    //   });
+    // }
     console.log('Data migration completed successfully.');
   } catch (error) {
     console.error('Error occurred during data migration:', error);
   } finally {
-    await application.close();
+    // await application.close();
     process.exit(0);
   }
 }
